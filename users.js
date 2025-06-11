27d84ca75e1cdef9911e55e81dd66edd2f3d2370
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2025-06-11 19:11:56 +0500

    Update

---
+++ users.js
@@ -0,0 +1,13 @@
+const users = [
+  {
+    id: '1',
+    name: 'Example user',
+    username: 'example-user',
+    email: 'example-user@onevizion.com',
+    password: '******',
+    attributes: {
+    }
+  }
+]
+
+module.exports = { users }
